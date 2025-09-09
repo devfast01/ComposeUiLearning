071850dd456ea1e5de77aca83ec5aa90c659ed33
devgast01 <iot123tech@gmail.com>
2025-09-09 11:16:31 +0500

    Quiz UI drawables are added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,5 +62,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation("androidx.constraintlayout:constraintlayout-compose-android:1.1.1")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 
 }
