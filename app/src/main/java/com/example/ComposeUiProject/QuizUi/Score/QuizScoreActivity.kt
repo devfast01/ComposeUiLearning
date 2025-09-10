66dfe510daac9e64e8c2de9345c7868063f76a29
devgast01 <iot123tech@gmail.com>
2025-09-10 19:17:51 +0500

    ScoreActivity is started

---
+++ app/src/main/java/com/example/ComposeUiProject/QuizUi/Score/QuizScoreActivity.kt
@@ -0,0 +1,16 @@
+package com.example.ComposeUiProject.QuizUi.Score
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ComposeUiProject.R
+
+class QuizScoreActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
