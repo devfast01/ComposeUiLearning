1f13a9e8a5a9f63c2669e0342019c6ce5c3a2d51
devgast01 <iot123tech@gmail.com>
2025-09-11 07:54:51 +0500

    LeaderScreen back button

--- app/src/main/java/com/example/ComposeUiProject/QuizUi/Leader/LeaderSceen.kt
+++ app/src/main/java/com/example/ComposeUiProject/QuizUi/Leader/LeaderSceen.kt
@@ -1,2 +1,47 @@
 package com.example.ComposeUiProject.QuizUi.Leader
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ComposeUiProject.QuizUi.Leader.Model.UserModel
+import com.example.ComposeUiProject.QuizUi.Leader.components.OnBackRow
+import com.example.ComposeUiProject.R
+
+
+@Composable
+fun LeaderScreen(
+    topUsers: List<UserModel>,
+    otherUsers: List<UserModel>,
+    onBackClick: () -> Unit,
+) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(R.color.grey_quiz)),
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        item {
+            OnBackRow(onBackClick)
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun LeaderScreenPreview() {
+    val topUsers = listOf(UserModel(4, "John Smith", "person1", 8589))
+    val otherUsers = listOf(
+        UserModel(8, "Michael Davis", "person2", 1085),
+        UserModel(9, "Sarah Wilson", "person3", 6489),
+        UserModel(10, "Sarah Wilson", "person4", 5045),
+    )
+
+    LeaderScreen(topUsers, otherUsers) { }
+}
---
+++ app/src/main/java/com/example/ComposeUiProject/QuizUi/Leader/components/onBackRow.kt
@@ -0,0 +1,53 @@
+package com.example.ComposeUiProject.QuizUi.Leader.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ComposeUiProject.R
+
+
+@Composable
+fun OnBackRow(onBack: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = { onBack() }) {
+            Icon(
+                painter = painterResource(R.drawable.back),
+                contentDescription = null
+            )
+        }
+
+        Spacer(Modifier.width(16.dp))
+
+        Text(
+            "Single Player",
+            fontSize = 20.sp,
+            color = colorResource(R.color.navy_blue),
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview
+@Composable
+fun OnBackRowPreview() {
+    OnBackRow(onBack = {})
+}
