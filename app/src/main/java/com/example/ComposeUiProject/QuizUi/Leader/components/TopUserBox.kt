80d44fc46db7e8ce50ae023db4492e779e8379fb
devgast01 <iot123tech@gmail.com>
2025-09-11 09:20:37 +0500

    TopUserBox was done

--- app/src/main/java/com/example/ComposeUiProject/QuizUi/Leader/components/TopUserBox.kt
+++ app/src/main/java/com/example/ComposeUiProject/QuizUi/Leader/components/TopUserBox.kt
@@ -6,18 +6,23 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -122,9 +127,28 @@ fun TopUserBox(
                     .fillMaxWidth()
                     .padding(vertical = 8.dp)
             )
-
-
-
+        }
+        Row(
+            modifier = Modifier
+                .height(30.dp)
+                .wrapContentWidth()
+                .background(
+                    brush = SolidColor(Color(color.toColorInt())),
+                    shape = RoundedCornerShape(50)
+                )
+                .padding(horizontal = 8.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.garnet),
+                contentDescription = null,
+                modifier = Modifier.size(16.dp)
+            )
+            Spacer(Modifier.width(8.dp))
+            Text(
+                text = user.score.toString(),
+                color = Color.White,
+            )
         }
     }
 }
