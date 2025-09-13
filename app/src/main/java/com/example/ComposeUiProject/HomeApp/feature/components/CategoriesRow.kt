895845d3652094ad2a5f5ff21de8283ee1237ee7
devgast01 <iot123tech@gmail.com>
2025-09-13 08:30:04 +0500

    CategoriesRow is started

---
+++ app/src/main/java/com/example/ComposeUiProject/HomeApp/feature/components/CategoriesRow.kt
@@ -0,0 +1,23 @@
+package com.example.ComposeUiProject.HomeApp.feature.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.ComposeUiProject.R
+
+
+@Composable
+fun CategoriesRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        CategoryItem(R.drawable.cat_1, "", Modifier.weight(1f))
+    }
+}
