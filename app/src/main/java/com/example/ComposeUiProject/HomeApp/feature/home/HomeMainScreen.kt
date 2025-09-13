32ebd9c06b54644b636e5e2304671f587f529cda
devgast01 <iot123tech@gmail.com>
2025-09-13 07:44:00 +0500

    SearchRow was added to HomeMainScreen

--- app/src/main/java/com/example/ComposeUiProject/HomeApp/feature/home/HomeMainScreen.kt
+++ app/src/main/java/com/example/ComposeUiProject/HomeApp/feature/home/HomeMainScreen.kt
@@ -2,7 +2,9 @@ package com.example.ComposeUiProject.HomeApp.feature.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -12,12 +14,13 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ComposeUiProject.HomeApp.feature.components.HeaderSection
+import com.example.ComposeUiProject.HomeApp.feature.components.SearchRow
 import com.example.ComposeUiProject.R
 
 
 @Composable
 @Preview
-fun MainScreen() {
+fun HomeMainScreen() {
 
     val items = remember {
         sampleProperties()
@@ -33,6 +36,11 @@ fun MainScreen() {
         item {
             HeaderSection()
         }
+        item {
+            Spacer(Modifier.height(16.dp))
+        }
+        item { SearchRow() }
+
     }
 
 }
