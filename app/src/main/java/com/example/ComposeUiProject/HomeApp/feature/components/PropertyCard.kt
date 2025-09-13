fca9e0b50b263e85224c5b1d6aa9139c73651722
devgast01 <iot123tech@gmail.com>
2025-09-13 09:09:07 +0500

    Meta chip is started

--- app/src/main/java/com/example/ComposeUiProject/HomeApp/feature/components/PropertyCard.kt
+++ app/src/main/java/com/example/ComposeUiProject/HomeApp/feature/components/PropertyCard.kt
@@ -1,18 +1,32 @@
 package com.example.ComposeUiProject.HomeApp.feature.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ComposeUiProject.HomeApp.core.model.PropertyHome
+import com.example.ComposeUiProject.HomeApp.util.getDrawableId
 import com.example.ComposeUiProject.R
 
 
@@ -31,7 +45,62 @@ fun PropertyCard(item: PropertyHome) {
             .fillMaxWidth()
             .clip(RoundedCornerShape(30.dp))
             .background(white)
-    ) { }
+    ) {
+        Box {
+            Image(
+                painter = painterResource(getDrawableId(item.pickPath)),
+                contentDescription = null, modifier = Modifier
+                    .fillMaxWidth()
+                    .background(white)
+                    .height(233.dp)
+                    .padding(12.dp)
+                    .clip(RoundedCornerShape(30.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                "$${item.price}",
+                color = colorResource(R.color.white),
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 28.dp, bottom = 28.dp)
+                    .align(Alignment.BottomStart)
+                    .clip(RoundedCornerShape(50.dp))
+                    .background(blue)
+                    .padding(horizontal = 16.dp, vertical = 6.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(30.dp))
+                    .padding(horizontal = 16.dp)
+            ) {
+                Spacer(Modifier.height(8.dp))
+                Text(
+                    item.title,
+                    color = black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+
+                Text(
+                    item.address,
+                    color = grey,
+                    fontSize = 13.sp,
+                )
+                Spacer(Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 12.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+
+                }
+            }
+        }
+    }
 }
 
 @Preview
@@ -41,7 +110,7 @@ fun PropertyCardPreview() {
         type = "Apartment",
         title = "Modern Apartment",
         address = "123 Main St, Anytown, USA",
-        pickPath = "apartment.jpg",
+        pickPath = "pic_1",
         price = 1200,
         bed = 2,
         bath = 2,
