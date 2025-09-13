76d561a9e00c111eaf7aa0a98a67eab69cedb154
devgast01 <iot123tech@gmail.com>
2025-09-13 09:27:46 +0500

    PropertyCard was done

--- app/src/main/java/com/example/ComposeUiProject/HomeApp/feature/home/HomeMainScreen.kt
+++ app/src/main/java/com/example/ComposeUiProject/HomeApp/feature/home/HomeMainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -15,6 +16,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ComposeUiProject.HomeApp.feature.components.CategoriesRow
 import com.example.ComposeUiProject.HomeApp.feature.components.HeaderSection
+import com.example.ComposeUiProject.HomeApp.feature.components.PropertyCard
 import com.example.ComposeUiProject.HomeApp.feature.components.SearchRow
 import com.example.ComposeUiProject.R
 
@@ -46,6 +48,14 @@ fun HomeMainScreen() {
         }
         item {
             CategoriesRow()
+        }
+
+        items(items) { property ->
+            PropertyCard(property)
+        }
+
+        item {
+            Spacer(Modifier.height(16.dp))
         }
     }
 
