36baf2cbaeac6d5529eb896362abea5bb76a1bab
devgast01 <iot123tech@gmail.com>
2025-09-14 20:50:53 +0500

    BottomNavBar was added to Ticket main screen

--- app/src/main/java/com/example/ComposeUiProject/TicketApp/Activities/Dashboard/TicketDashboardActivity.kt
+++ app/src/main/java/com/example/ComposeUiProject/TicketApp/Activities/Dashboard/TicketDashboardActivity.kt
@@ -5,11 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ComposeUiProject.R
+import com.example.ComposeUiProject.TicketApp.Activities.Splash.StatusTopBarColor
 
+@Suppress("DEPRECATION")
 class TicketDashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +30,25 @@ class TicketDashboardActivity : ComponentActivity() {
         enableEdgeToEdge()
         window.statusBarColor = ContextCompat.getColor(this, R.color.grey_quiz)
         setContent {
-
+            TicketMainScreen()
         }
+    }
+}
+
+@Composable
+@Preview
+fun TicketMainScreen() {
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        bottomBar = { MyBottomBar() },
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = colorResource(R.color.darkPurple2_ticket))
+                .padding(paddingValues)
+        ) { }
     }
 }
