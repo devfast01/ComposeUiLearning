086cda2e45a842d7df99e12a16fd2f5a1cff960d
devgast01 <iot123tech@gmail.com>
2025-09-15 11:47:19 +0500

    Working on DropDownList

--- app/src/main/java/com/example/ComposeUiProject/TicketApp/Activities/Dashboard/DropDownList.kt
+++ app/src/main/java/com/example/ComposeUiProject/TicketApp/Activities/Dashboard/DropDownList.kt
@@ -2,29 +2,34 @@
 
 package com.example.ComposeUiProject.TicketApp.Activities.Dashboard
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ComposeUiProject.R
 
-
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DropDownMenu(
+fun DropDownList(
     items: List<String>,
     loadingIcon: Painter,
     hint: String = "",
@@ -63,8 +68,25 @@ fun DropDownMenu(
                     .clip(RoundedCornerShape(10.dp))
                     .menuAnchor(),
                 placeholder = {
+                    Text(
+                        text = hint,
+                        color = Color.Black,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                },
+                leadingIcon = {
+                    Image(painter = loadingIcon, contentDescription = null)
+                },
+                textStyle = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 14.sp,
+                ),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent,
+                )
 
-                }
             )
         }
 
