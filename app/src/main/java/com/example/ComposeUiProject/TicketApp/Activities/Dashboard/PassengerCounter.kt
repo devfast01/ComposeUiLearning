d06969e82de17ad77ed036b26568f812580da8df
devgast01 <iot123tech@gmail.com>
2025-09-15 13:15:43 +0500

    Working on PasssengerCounter

--- app/src/main/java/com/example/ComposeUiProject/TicketApp/Activities/Dashboard/PassengerCounter.kt
+++ app/src/main/java/com/example/ComposeUiProject/TicketApp/Activities/Dashboard/PassengerCounter.kt
@@ -1,2 +1,92 @@
 package com.example.ComposeUiProject.TicketApp.Activities.Dashboard
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ComposeUiProject.R
+
+@Composable
+fun PassengerCounter(
+    title: String,
+    modifier: Modifier = Modifier,
+    onItemSelected: (String) -> Unit,
+) {
+    var passengerCount by remember { mutableIntStateOf(1) }
+
+    Box(
+        modifier = Modifier
+            .height(16.dp)
+            .padding(top = 8.dp)
+            .background(
+                color = colorResource(R.color.lightPurple_ticket),
+                shape = RoundedCornerShape(10.dp)
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.passenger_ic),
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(Modifier.width(8.dp))
+
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .clickable {
+                        if (passengerCount > 1) {
+                            passengerCount--
+                            onItemSelected(passengerCount.toString())
+                        }
+                    }, contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "-",
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+
+            Text(
+                text = "-",
+                color = Color.Black,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
+
+    }
+}
