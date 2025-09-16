8118c77124b01fa1568fb65e30846124a7928603
devgast01 <iot123tech@gmail.com>
2025-09-16 15:59:48 +0500

    Working on TicketHeader

---
+++ app/src/main/java/com/example/ComposeUiProject/TicketApp/Activities/SeatSelect/TicketHeader.kt
@@ -0,0 +1,68 @@
+package com.example.ComposeUiProject.TicketApp.Activities.SeatSelect
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.ComposeUiProject.R
+
+
+@Composable
+fun TicketDetailHeader(
+    modifier: Modifier,
+    onBackClick: () -> Unit,
+) {
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = colorResource(R.color.darkPurple2_ticket))
+            .padding(top = 45.dp, start = 16.dp, end = 16.dp)
+    ) {
+        val (backBtn, headerTitle, worldImg) = createRefs()
+        Image(
+            painter = painterResource(R.drawable.back_home),
+            contentDescription = null,
+            modifier = Modifier
+                .clickable { onBackClick() }
+                .constrainAs(backBtn) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                }
+        )
+        Text(
+            text = "Choose Seats",
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .constrainAs(headerTitle) {
+                    top.linkTo(backBtn.top)
+                    start.linkTo(backBtn.end, margin = 8.dp)
+                    bottom.linkTo(backBtn.bottom)
+                },
+        )
+        Image(
+            painter = painterResource(R.drawable.world),
+            contentDescription = null,
+            modifier = Modifier
+                .constrainAs(worldImg) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                }
+        )
+    }
+}
